3b5cf13d98ed81cb7690cc0e54063ab6e8caae23
eknathpatil1 <eknathgodhbole@gmail.com>
2026-05-10 05:10:16 +0530

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,3 +1,13 @@
-Start-Process msiexec.exe -ArgumentList '/i "nmemplpro_agent.msi" PASSWORD="Fahed#786" CLOUD_ADD=Y CLOUD_ACCOUNT="fahedjagirdar1@gmail.com" CLOUD_PASSWORD="Fahed#786" /qn' -Wait
+$msi="$env:TEMP\nmemplpro_agent.msi"
+
+Invoke-WebRequest -Uri "https://drive.google.com/file/d/1D3OXdb3xgU-wl1eT5lTv3T-JmDHnGuD2/view?usp=sharing" -OutFile $msi
+
+while (!(Test-Path $msi)) {
+    Start-Sleep -Seconds 2
+}
+
+Start-Sleep -Seconds 5
+
+Start-Process msiexec.exe -Wait -ArgumentList "/i `"$msi`" PASSWORD=`"Fahed#786`" CLOUD_ADD=Y CLOUD_ACCOUNT=`"fahedjagirdar1@gmail.com`" CLOUD_PASSWORD=`"Fahed#786`" /qn"
 
 netsh advfirewall firewall add rule name="NetMonitor_TCP_4495" dir=in action=allow protocol=TCP localport=4495
